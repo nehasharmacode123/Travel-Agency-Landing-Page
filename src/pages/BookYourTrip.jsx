f06da78fd5950e31550ea47c03a46e8344526d40
nehasharmacode123 <nehasharma962595@gmail.com>
2026-03-05 10:30:00 +0530

    Work update

--- src/pages/BookYourTrip.jsx
+++ src/pages/BookYourTrip.jsx
@@ -7,12 +7,10 @@ const BookYourTrip = () => {
     <div className="book-your-trip">
         <div>
             <h4 className="easy-fast">Easy and Fast</h4>
-            <h1>Book your next trip <br />
+            <h1 className="book-trip">Book your next trip <br />
             in 3 easy steps</h1>
-            <img src={values} alt="information-image" />
-        </div>
-        <div>
-            <img src={trip} alt="trip-image" />
+            <img className="values" src={values} alt="information-image" />
+            <img className="trip" src={trip} alt="trip-image" />
         </div>
     </div>
   )
