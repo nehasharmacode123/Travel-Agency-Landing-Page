feafc1299afa0d3d141a0129208eff0f53b148be
nehasharmacode123 <nehasharma962595@gmail.com>
2026-03-09 11:21:46 +0530

    create category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,6 +3,8 @@ import settlelight from "../assets/settlelight.png";
 import plane from "../assets/plane.png";
 import mic from "../assets/mic.png";
 import settings from "../assets/settings.png";
+import Rectangle157 from "../assets/Rectangle157.png";
+import Group4 from "../assets/Group4.png";
 
 const Category = () => {
   return (
@@ -10,13 +12,15 @@ const Category = () => {
         <div>
             <h1 className="category">CATEGORY</h1>
             <h2 className="offer">We Offer Best Services</h2>
+            <img className="side-design-image" src={Group4} alt="side-design-image" />
         </div>
-        {/* <div>
-            <img src={settlelight} alt="settlelight-picture" />
-            <img src={plane} alt="plane-picture" />
-            <img src={mic} alt="mic-picture" />
-            <img src={settings} alt="settings-picture" />
-        </div> */}
+        <div className="box">
+            <img className="rectangle" src={Rectangle157} alt="rectangle157" />
+            <img className="settlelight" src={settlelight} alt="settlelight-picture" />
+            <img className="plane" src={plane} alt="plane-picture" />
+            <img className="mic" src={mic} alt="mic-picture" />
+            <img className="settings" src={settings} alt="settings-picture" />
+        </div>
     </div>
   )
 }
