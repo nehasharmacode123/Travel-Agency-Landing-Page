53d20240eaec0d99d3b225cef5380dd5d7b8b234
nehasharmacode123 <nehasharma962595@gmail.com>
2026-03-10 14:42:21 +0530

    add email icon

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,17 +1,27 @@
 import "./form.css";
 import rectangle164 from "../assets/rectangle164.png";
 import circle from "../assets/circle.png";
+import { MdOutlineMailOutline } from "react-icons/md";
+
 const Form = () => {
   return (
-    <div>
-        <div>
-            <img className="rectangle-shape" src={rectangle164} alt="" />
-            <p>Subscribe to get information, latest news and other <br />
-                interesting offers about Jadoo</p>
-                <img src={circle} alt="circle-image" />
-                <input type="text" placeholder="Your email" />
-                <button>Subscribe</button>
+    <div className="form-container">
+      <img className="rectangle-shape" src={rectangle164} alt="rectangle-shape-image" />
+      <img className="circle" src={circle} alt="circle-image" />
+
+      <div className="input-box">
+        <p>
+          Subscribe to get information, latest news and other <br />
+          interesting offers about Jadoo
+        </p>
+        <div className="email-input">
+          <MdOutlineMailOutline className="mail-icon" />
+          <input type="text" placeholder="Your email" />
         </div>
+        <input type="text" placeholder="Your email" />
+        
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
