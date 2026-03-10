e20335d21524030fdbe9f896a73c048b4721cc1c
nehasharmacode123 <nehasharma962595@gmail.com>
2026-03-10 10:33:50 +0530

    working testimonials page

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,6 +1,11 @@
 import "./testimonial.css";
 import Group64 from "../assets/Group64.png";
 import Group61 from "../assets/Group61.png";
+import axon from "../assets/axon.png";
+import jetstar from "../assets/jetstar.png";
+import expedia from "../assets/expedia.png";
+import qantus from "../assets/qantus.png";
+import fook from "../assets/fook.png";
 
 const Testimonial = () => {
   return (
@@ -12,6 +17,15 @@ const Testimonial = () => {
         </div>
         <div>
             <img className="group" src={Group64} alt="testimonials" />
+        </div>
+        <div className="company-logo">
+          <div className="logo1">
+              <img className="axon" src={axon} alt="axon-company-logo" />
+              <img className="jetstar" src={jetstar} alt="jetstar-company-logo" />
+              <img className="expedia" src={expedia} alt="expedia-company-logo" />
+              <img src={qantus} alt="qantus-company-logo" />
+              <img src={fook} alt="fook-company-logo" />
+          </div>
         </div>
     </div>
   )
