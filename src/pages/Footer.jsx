521c717a8aa86bd558548878ce303fe076edd08c
nehasharmacode123 <nehasharma962595@gmail.com>
2026-03-11 19:30:00 +0530

    Project update

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,11 +1,41 @@
 import "./footer.css";
 import CompanyDesc from "../assets/CompanyDesc.png";
+import outbound from "../assets/outbound.png";
 
 const Footer = () => {
   return (
     <div>
-        <div>
+        <div className="footer-page">
+          <div className="footer-section">
             <img className="companyDesc" src={CompanyDesc} alt="Company-logo" />
+          </div>
+
+          <div>
+            <h3>Company</h3>
+            <p>About</p>
+            <p>Careers</p>
+            <p>Mobile</p>
+          </div>
+
+          <div>
+            <h4>Contact</h4>
+            <p>Help/FAQ</p>
+            <p>Press</p>
+            <p>Affilates</p>
+          </div>
+
+          <div>
+            <h4>More</h4>
+            <p>Airlinefees</p>
+            <p>Airline</p>
+            <p>Low fare tips</p>
+          </div>
+
+          <div>
+            <img src={outbound} alt="outbound-image" />
+            <p>All rights reserved@jadoo.co</p>
+          </div>
+
         </div>
     </div>
   )
