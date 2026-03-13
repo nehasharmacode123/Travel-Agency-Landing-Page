5d43e7de3fcb95633a728df7c14111c53de68d15
nehasharmacode123 <nehasharma962595@gmail.com>
2026-03-13 11:09:16 +0530

    improve layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Category from "./pages/Category"
 import Destination from "./pages/Destination"
 import Footer from "./pages/Footer"
 import Form from "./pages/Form"
+// import Nav from "./pages/Nav"
 import Navbar from "./pages/navbar"
 import Testimonial from "./pages/Testimonial"
 
 const App = () => {
   return (
     <div>
+      {/* <Nav /> */}
       <Navbar />
       <Category />
       <Destination /> 
